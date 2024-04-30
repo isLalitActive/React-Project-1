357d0d35df7fce8e6a4e26336dfe44190bb61104
isLalitActive <lalit.postemails@gmail.com>
2024-04-30 11:49:36 +0530

    created a parent-child structure using React

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const heading = React.createElement('h1', {
-    id : 'heading',
-    name: 'title'
-}, 'Hello from React');
-const root = ReactDOM.createRoot(document.getElementById('root-container'));
+/*
+Create this html structure using React
+    <div id='parent'>
+        <div id='child'>
+            <h1>I'm h1 tag</h1>
+        </div>
+    </div>
+*/
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+    name: "title",
+  },
+  "Hello from React"
+);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root-container"));
 root.render(heading);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
+console.log(parent);
+
+const root2 = ReactDOM.createRoot(document.getElementById("parent-container"));
+root2.render(parent);
