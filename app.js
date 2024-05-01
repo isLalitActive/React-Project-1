2d4af01de22895d79ceafa2a00f9d17369032026
isLalitActive <lalit.postemails@gmail.com>
2024-05-02 01:26:19 +0530

    Rendering html using JSX not using React.createElement

--- app.js
+++ app.js
@@ -1,43 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/*
-Create this html structure using React
-    <div id='parent'>
-        <div id='child-1'>
-            <h1>I'm h1 tag</h1>
-            <h2>I am h2 tag</h2>
-        </div>
-        <div id='child-2'>
-            <h1>I'm h1 tag</h1>
-            <h2>I am h2 tag</h2>
-        </div>
-    </div>
-*/
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-    name: "title",
-  },
-  "Hello from React"
-);
-console.log(heading);
+/*We're creating html structure using React*/
+const heading = React.createElement('h1', {id: 'heading'}, 'This heading is from React.createElement'); 
+const root = ReactDOM.createRoot(document.getElementById('root-container'));
 
-const root = ReactDOM.createRoot(document.getElementById("root-container"));
-root.render(heading);
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child-1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child-2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
-console.log(parent);
+/*Create html structure using JSX*/
+const message = <h1 id="message">This message is coming from JSX</h1>
 
-const root2 = ReactDOM.createRoot(document.getElementById("parent-container"));
-root2.render(parent);
+root.render(message);
+
