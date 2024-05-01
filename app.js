46c33256c1fd04828dc3d2f33fc5c2413892230e
isLalitActive <lalit.postemails@gmail.com>
2024-05-02 02:04:13 +0530

    JSX is not HTML

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-/*We're creating html structure using React*/
-const heading = React.createElement('h1', {id: 'heading'}, 'This heading is from React.createElement'); 
-const root = ReactDOM.createRoot(document.getElementById('root-container'));
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /*Create html structure using JSX*/
-const message = <h1 id="message">This message is coming from JSX</h1>
+const message = (
+  <h1 className="heading" tabIndex="2">
+    This is coming from JSX
+  </h1>
+);
+const root = ReactDOM.createRoot(document.getElementById("root-container"));
 
 root.render(message);
-
