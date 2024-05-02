bd44df64ada2cdcf73f0dd6eceb920607495a472
isLalitActive <lalit.postemails@gmail.com>
2024-05-02 14:15:52 +0530

    introduction to react component

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*Create html structure using JSX*/
-const message = (
+/*Create html structure using JSX: React Element*/
+const message1 = <h1 className="heading" tabIndex='2'>This is coming from JSX, Its a function only</h1>;
+
+//React Element
+const message2 = (
   <h1 className="heading" tabIndex="2">
-    This is coming from JSX
+    This is coming from JSX as well, and both approach are correct
   </h1>
 );
+
+const Heading = () => (
+  <div className="heading-container">
+    <h1>This is coming from React Component as well</h1>
+  </div>
+);
+
+const Message = () => (
+  <div id="div-container">
+    <Heading />
+    <h1 className="react-heading" tabIndex='2'>This is coming from React Component</h1>
+  </div>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root-container"));
 
-root.render(message);
+root.render(<Message />);
